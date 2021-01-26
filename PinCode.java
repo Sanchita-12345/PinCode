a08269faaad1fb919751c891736265c6c635f6f8
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-01-25 21:26:29 -0800

    PinCode problem part 4 done

--- PinCode.java
+++ PinCode.java
@@ -4,8 +4,8 @@ class PinCode{
 
 	public static void main(String args[]){
 
-		Pattern pat = Pattern.compile("^[0-9]{6}");
-		Matcher match = pat.matcher("400088B");
+		Pattern pat = Pattern.compile("^[0-9]{3}\\s[0-9]{3}$");
+		Matcher match = pat.matcher("400 088");
 		boolean result = match.matches();
 		System.out.println(result);
 	}
